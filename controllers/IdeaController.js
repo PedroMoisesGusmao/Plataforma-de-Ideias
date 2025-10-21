5b597e8d40ab649f4790dd7b7c6dab72d5228169
Pedro <pedroaraujo4910@gmail.com>
2025-10-21 10:12:21 -0300

    Implement initial structure for Idea and User controllers and routes

---
+++ controllers/IdeaController.js
@@ -0,0 +1,13 @@
+module.exports = {
+    getAllIdeas: (req, res) => {
+    },
+
+    saveIdea: (req, res) => {
+    },
+
+    updateIdea: (req, res) => {
+    },
+
+    deleteIdea: (req, res) => {
+    }
+}
